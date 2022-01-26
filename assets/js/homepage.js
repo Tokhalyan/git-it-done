301d445dd2f05a62e06a87c8939c5f8ef1959765
Shahen Tokhalyan <tokhalyan@gmail.com>
2022-01-25 18:16:26 -0800

    function created for viewing different repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-let getUserRepos = function() {
-    fetch("https://api.github.com/users/Tokhalyan/repos");
-};
+let getUserRepos = function(user) {
+    // format the github api url
+    let apiUrl = `https://api.github.com/users/${user}/repos`;
 
-getUserRepos();
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data){
+            console.log(data);
+        })
+    })
+} 
+
+getUserRepos("apple");
